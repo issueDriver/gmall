85a674754d36ee3f235bf02bd1c378435b31a6a2
yangjie <yangjie@doweidu.com>
2019-11-05 21:58:50 +0800

    引入duubo服务

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # gmall
+
+## gmall-user 9090
+
+gmall-user-service 用户服务的service层8070
+
+gmall-user-web 用户服务的web层9091
---
+++ gmall-user-web/src/main/java/com/zuojie/gmall/user/controller/UserController.java
@@ -0,0 +1,42 @@
+package com.zuojie.gmall.user.controller;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.zuojie.gmall.bean.UmsMember;
+import com.zuojie.gmall.bean.UmsMemberReceiveAddress;
+import com.zuojie.gmall.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+@Controller
+public class UserController {
+
+    @Reference
+    UserService userService;
+
+    @RequestMapping("getReceiveAddressByMemberId")
+    public List<UmsMemberReceiveAddress> getReceiveAddressByMemberId(String memberId){
+
+        List<UmsMemberReceiveAddress> umsMemberReceiveAddresses = userService.getReceiveAddressByMemberId(memberId);
+
+        return umsMemberReceiveAddresses;
+    }
+
+
+    @RequestMapping("getAllUser")
+    @ResponseBody
+    public List<UmsMember> getAllUser(){
+
+        List<UmsMember> umsMembers = userService.getAllUser();
+
+        return umsMembers;
+    }
+
+    @RequestMapping("index")
+    @ResponseBody
+    public String index(){
+        return "hello user";
+    }
+
+}
